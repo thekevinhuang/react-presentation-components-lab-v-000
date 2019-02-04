6475eddc8db080e1b1c1c40c6d383598e953afdb
Kevin Huang <yellow.khaos@gmail.com>
2019-02-04 21:35:56 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -2,5 +2,18 @@
 import React, {Component} from 'react'
 
 export default class SimpleComponent extends Component {
+  constructor () {
+    super()
+    this.state = {
+      mood = "happy"
+    }
+  }
 
+  render () {
+    return (
+      <div>
+        {this.state.mood}
+      </div>
+    )
+  }
 }
