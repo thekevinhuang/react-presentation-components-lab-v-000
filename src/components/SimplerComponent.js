d9bbf6e58c4e27b6386741cac4b1ad4ae220a520
Kevin Huang <yellow.khaos@gmail.com>
2019-02-04 22:08:37 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,5 +1,3 @@
 // Code SimplerComponent Here
-const SimplerComponent = ({props}) =>
-  <div onClick={props.handleClick}>
-    I am just happy
-  </div>
+const SimplerComponent = ({props}) => <div onClick={props.handleClick}>I am just happy</div>
+
