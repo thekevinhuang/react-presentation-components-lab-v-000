ceeda2e9b757a0ca7daa4acf81822d3d04156582
Kevin Huang <yellow.khaos@gmail.com>
2019-02-04 22:00:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+fun = (event) => {
+  alert("hello!")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.fun} />
   </div>,
   document.getElementById('root')
 );
